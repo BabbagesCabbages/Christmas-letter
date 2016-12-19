f34b297e923e13f29e23a48c15d2cbe4ec50c76d
Karson Madden <krm@karsonm.local>
2016-12-19 11:55:01 -0800

    initial commit

---
+++ src/javascripts/app.js
@@ -0,0 +1,10 @@
+import './modules'
+
+console.log(`app.js has loaded!`)
+
+
+// $('#bored').bind('click', function(event) { 
+//     console.log('clicked');
+//     impress().goto( document.getElementById('title') )
+//     event.stopPropagation();
+// })
